96cc2f1efff5d7d37721c851d74b16c1f1317832
LoulouNoLegend <40952934+LoulouNoLegend@users.noreply.github.com>
2024-12-02 13:20:39 -0500

    Added JSON DB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Windows;
 
+
 namespace ModManager
 {
     public partial class MainWindow : Window {
@@ -11,21 +12,30 @@ namespace ModManager
 
         public MainWindow() {
             InitializeComponent();
+            Mods = new ObservableCollection<Mod>(JsonCacheManager.LoadMods());
             ModsDataGrid.ItemsSource = Mods;
         }
 
+
         private void AddModsButton_Click(object sender, RoutedEventArgs e) {
-            var openFileDialog = new OpenFileDialog {
+            var openFileDialog = new OpenFileDialog
+            {
                 Filter = "Compressed Files (*.zip;*.rar;*.7z)|*.zip;*.rar;*.7z",
                 Multiselect = true
             };
 
-            if (openFileDialog.ShowDialog() == true) {
-                foreach (var filePath in openFileDialog.FileNames) {
-                    try {
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (var filePath in openFileDialog.FileNames)
+                {
+                    try
+                    {
                         var mod = ArchiveHandler.ExtractMod(filePath);
                         Mods.Add(mod);
-                    } catch (Exception ex) {
+                        JsonCacheManager.SaveMods(Mods);
+                    }
+                    catch (Exception ex)
+                    {
                         MessageBox.Show($"Failed to add mod: {ex.Message}");
                     }
                 }
@@ -33,17 +43,25 @@ namespace ModManager
         }
 
         private void RemoveModsButton_Click(object sender, RoutedEventArgs e) {
-            var selectedMods = ModsDataGrid.SelectedItems;
-            for (var i = Mods.Count - 1; i >= 0; i--) { 
-                var mod = Mods[i];
-                if (selectedMods.Contains(mod)) {
-                    Mods.Remove(mod);
-                }
+            var selectedMods = ModsDataGrid.SelectedItems.Cast<Mod>().ToList();
+            foreach (var mod in selectedMods)
+            {
+                Mods.Remove(mod);
             }
+            JsonCacheManager.SaveMods(Mods);
         }
 
         private void ExportProfileButton_Click(object sender, RoutedEventArgs e) {
             MessageBox.Show("Export Profile feature is not implemented yet.");
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Settings menu is not implemented yet.");
+
+            // Open the Settings window
+            //Settings settingsWindow = new Settings();
+            //settingsWindow.Show();
         }
     }
 }
--- ModManager.cs
+++ ModManager.cs
@@ -6,5 +6,6 @@
         public double Size { get; set; } // Size in MB
         public string Type { get; set; }
         public bool IsEnabled { get; set; } = false;
+        public List<string> FilePaths { get; set; } = new List<string>();
     }
 }
