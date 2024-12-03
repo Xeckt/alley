7e853f6fe5d0668757fdabc5fc8ede607a49f7df
Node <jordansavell1198@gmail.com>
2024-12-03 15:22:17 +0000

    remove unnecessary code and add comments

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -70,18 +70,12 @@ namespace ModManager
 
         private void Enabled_Checked(object sender, RoutedEventArgs e)
         {
-            if (sender is CheckBox checkBox && checkBox.DataContext is Mod mod)
-            {
-                MessageBox.Show($"Mod '{mod.Name}' has been enabled.");
-            }
+            // Add Logic
         }
 
         private void Enabled_Unchecked(object sender, RoutedEventArgs e)
         {
-            if (sender is CheckBox checkBox && checkBox.DataContext is Mod mod)
-            {
-                MessageBox.Show($"Mod '{mod.Name}' has been disabled.");
-            }
+            // Add Logic
         }
     }
 }
