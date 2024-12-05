1b18d4d28f391a5a933e0b970fbff235895475a0
Node <jordansavell1198@gmail.com>
2024-12-05 12:49:00 +0000

    fix cache saving functionality

--- Handler/CacheHandler.cs
+++ Handler/CacheHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml;
@@ -8,52 +9,56 @@ namespace ModManager.Handler
 {
     public static class CacheHandler
     {
-        // Path to the JSON file that stores the mod cache
         private static readonly string _filePath = "modcache.json";
 
-        /// Loads the mods from the JSON file
-        /// If the file doesn't exist, it returns an empty list
         public static List<Mod> LoadMods()
         {
-            // Check if the JSON file exists
             if (!File.Exists(_filePath))
             {
-                return new List<Mod>(); // Return an empty list if no file exists
+                return new List<Mod>();
             }
 
-            // Read the JSON content from the file
             var json = File.ReadAllText(_filePath);
 
-            // Deserialize the JSON content into a ModCache object
             var cache = JsonConvert.DeserializeObject<ModCache>(json);
 
-            // Return the list of mods or an empty list if null
             return cache?.Mods ?? new List<Mod>();
         }
 
-        /// Saves the given list of mods to the JSON file
-        /// If it exists, it will overwritte
         public static void SaveMods(IEnumerable<Mod> mods)
         {
-            // Wrap the list of mods in a ModCache object
-            var cache = new ModCache { Mods = mods.ToList() };
+            List<Mod> savedMods;
 
-            // Serialize the ModCache object into a JSON string
-            var json = JsonConvert.SerializeObject(cache, Newtonsoft.Json.Formatting.Indented);
+            if (File.Exists(_filePath))
+            {
+                var cacheContent = File.ReadAllText(_filePath);
+                var savedCache = JsonConvert.DeserializeObject<ModCache>(cacheContent);
+                savedMods = savedCache?.Mods ?? new List<Mod>();
+            } 
+            else
+            {
+                savedMods = new List<Mod>();
+            }
 
-            // Write the JSON string to the file
-            File.AppendAllText(_filePath, json);
+            foreach (var mod in mods)
+            {
+                if (!savedMods.Exists(m => m.Name == mod.Name))
+                {
+                    savedMods.Add(mod);
+                }
+            }
 
-            foreach (var mod in mods) {
-                if (!Mod.ModsList.Any(existingMod => existingMod.Name == mod.Name)) // Check if the same mod is currently loaded in the list
-                { 
+            File.WriteAllText(_filePath, JsonConvert.SerializeObject(new ModCache { Mods = savedMods }, Newtonsoft.Json.Formatting.Indented));
+
+            foreach (var mod in savedMods)
+            {
+                if (!Mod.ModsList.Any(existingMod => existingMod.Name == mod.Name))
+                {
                     Mod.ModsList.Add(mod);
                 }
             }
         }
 
-        /// Private class to wrap the mods for JSON
-        /// It ensures the structure matches the JSON file format
         private class ModCache
         {
             public List<Mod>? Mods { get; set; }
