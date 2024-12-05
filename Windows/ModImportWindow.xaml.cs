0cf8654316889497a6382ce90e0a8577dbb59fa0
Node <jordansavell1198@gmail.com>
2024-12-05 10:26:12 +0000

    Add new window for mod importing

---
+++ Windows/ModImportWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using ModManager.Handler;
+using SharpCompress.Common;
+
+namespace ModManager.Windows
+{
+    /// <summary>
+    /// Interaction logic for ModImportWindow.xaml
+    /// </summary>
+    public partial class ModImportWindow : Window
+    {
+        public ModImportWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void SelectModsButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Compressed Files (*.zip;*.rar;*.7z)|*.zip;*.rar;*.7z",
+                Multiselect = true
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                foreach (var filePath in openFileDialog.FileNames)
+                {
+                    try
+                    {
+                        ModsListBox.Items.Add(filePath);
+                    } 
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Failed to add mod to list: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void RemoveSelectedButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ModsListBox.SelectedItems.Cast<object>().ToList();
+
+            if (ModsListBox.SelectedItems.Count > 0 && ModsListBox.SelectedItems != null) 
+            {
+                foreach (var mod in selectedItems)
+                {
+                    ModsListBox.Items.Remove(mod);
+                }
+            }
+        }
+    }
+}
