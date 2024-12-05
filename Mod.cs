85d5349d45d9877b46b2da399fe4c7c9f7bf400b
Node <jordansavell1198@gmail.com>
2024-12-05 10:27:23 +0000

    rename modmanager to mod.cs, add enum and static observable collection

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ModManager
+{
+    public class Mod
+    {
+        public string Name { get; set; }
+        public double Size { get; set; } // Size in MB
+        public ModType Type { get; set; }
+        public bool IsEnabled { get; set; } = false;
+        public List<string> FilePaths { get; set; } = new List<string>();
+        public static ObservableCollection<Mod> ModsList { get; set; }
+    }
+
+    public enum ModType 
+    {
+        Map,
+        Clothing,
+        Utility,
+        Custom
+    }
+}
